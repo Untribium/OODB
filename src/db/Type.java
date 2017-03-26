7a473681e1270572785aaf3eb05dd6187979093b
martipa <martipa@student.ethz.ch>
2017-03-27 00:39:18 +0200

    init

---
+++ src/db/Type.java
@@ -0,0 +1,45 @@
+package db;
+
+public class Type {
+    private String name;
+
+    Type(String name) {
+        this.name = name;
+    }
+
+    public static Type newType(String name) {
+        return new Type(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Instance newInstance() {
+        return null;
+    }
+
+    /*public Set<Instance> allInstances() {
+        return null;
+    }
+
+    public Set<Instance> where(String attribute, int value) {
+
+    }
+
+    public Set<Instance> where(String attribute, double value) {
+
+    }
+
+    public Set<Instance> where(String attribute, String value) {
+
+    }
+
+    public Set<Instance> where(String attribute, Instance value) {
+
+    }*/
+
+    class Instance {
+
+    }
+}
---
+++ src/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+import db.Type;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("demo.demo");
+
+        Type t = new Type("test");
+
+
+    }
+
+}
